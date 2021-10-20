79eca2b4e4b02e6d5fde30cab95526f19ce1bf01
elias igbalajobi <heeleeaz@gmail.com>
2021-10-20 05:57:23 +0100

    deploy ready

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
-docker build -t heeleeaz/multi-client -f ./client/Dockerfile ./client
-docker build -t heeleeaz/multi-server -f ./client/Dockerfile ./server
-docker build -t heeleeaz/multi-worker -f ./client/Dockerfile ./worker
-docker push heeleeaz/multi-client
-docker push heeleeaz/multi-server
-docker push heeleeaz/multi-worker
+docker build -t heeleeaz/multi-client:latest -t heeleeaz/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t heeleeaz/multi-server:latest -t heeleeaz/multi-server:$SHA -f ./client/Dockerfile ./server
+docker build -t heeleeaz/multi-worker:latest -t heeleeaz/multi-worker:$SHA -f ./client/Dockerfile ./worker
+
+docker push heeleeaz/multi-client:latest
+docker push heeleeaz/multi-server:latest
+
+docker push heeleeaz/multi-worker:latest
+docker push heeleeaz/multi-client:$SHA
+docker push heeleeaz/multi-server:$SHA
+docker push heeleeaz/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=heeleeaz/multi-server
+kubectl set image deployments/server-deployment server=heeleeaz/multi-server:$SHA
+kubectl set image deployments/client-deployment client=heeleeaz/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=heeleeaz/multi-worker:$SHA
